a5c9b372ce0452eaeba2ba61ce0900a63a6c575e
topette <oscarleonardo.osorio@gmail.com>
2022-07-22 22:18:56 -0500

    añadiendo request js

---
+++ js/request.js
@@ -0,0 +1,22 @@
+API_URL = 'https://my-json-server.typicode.com/topette/M3U2TrelloApp-topette/db'
+
+const ApiClient = axios.create({
+    baseURL: API_URL
+})
+
+ApiClient.get(`${API_URL}/tasks`)
+    .then((res) => mostrarTareas(res.data))
+    .catch((err) => console.error (err))
+
+const mostrarTareas = (data) => {
+    data.map((tarea) => crearTarea(tarea))
+}
+
+const crearTarea = (tarea) => {
+    let nuevaTarea = document.createElement('article')
+    nuevaTarea.classList.add('card-tarea')
+
+    let tituloTarea = document.createElement('h3')
+    nuevaTarea.classList.add('card-tarea')
+}
+
